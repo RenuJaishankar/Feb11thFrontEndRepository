24d2ae368af5f8997ddb67153ce57dccabe8044c
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-12 15:31:44 -0500

    modified project front end files

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const myFunc = () => {
-    fetch('https://still-thicket-63557.herokuapp.com/data', { //restful endpoint
-      
-    })
+    //fetch('https://still-thicket-63557.herokuapp.com/data', { //restful endpoint
+    fetch('https://still-thicket-63557.herokuapp.com/fruits/type')
     .then(response => response.json())
   //.then(json => console.log(json))
-    .then(json => document.getElementById("apples").innerHTML = `there are ${json.amount} ${json.name}`)
+   // .then(json => document.getElementById("apples").innerHTML = `there are ${json.amount} ${json.name}`)
+    .then(json => document.getElementById("fruits").innerHTML = `there are ${json.type} ${json.name}`)
+
 }
+
+ const myNewFunc = () => {
+  fetch('https://still-thicket-63557.herokuapp.com/hi')
+   .then(response => response.json())
+ .then(json => document.getElementById().innerHTML = `says hi`)
+   }
